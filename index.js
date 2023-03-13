0bd6f2d4f499e48882cfb52f9a960b1d26c73ff9
hammad rafique <hammadrafique397@gmail.com>
2023-03-13 23:58:23 +0500

    debouncing created

--- index.js
+++ index.js
@@ -1,30 +1,31 @@
-'use strict';
-
-
-
-const button = document.querySelector('button');
+const button = document.querySelector("button");
 
 let option;
 
 const debounce = (func, delay, option = { leading: false, trailing: true }) => {
-    let timeoutId = null;
-    return (...args) => {
-        let isInvoked = false;
-        if(timeoutId === null && option.leading){
-            func.call(this, ...args);
-        }
-        clearTimeout(timeoutId);
-        timeoutId = setTimeout(() => {
-            if(option.trailing && !isInvoked){
-                func.call(this, ...args);
-            }
-            timeoutId = null;
-        }, delay)
+  let timeoutId = null;
+  return (...args) => {
+    let isInvoked = false;
+    if (timeoutId === null && option.leading) {
+      func.call(this, ...args);
+      isInvoked = true;
     }
-}
+    clearTimeout(timeoutId);
+    timeoutId = setTimeout(() => {
+      if (option.trailing && !isInvoked) {
+        func.call(this, ...args);
+      }
+      timeoutId = null;
+    }, delay);
+  };
+};
 
-const clickMe = () => console.log('Added to cart');
+const clickMe = () => console.log("Added to cart");
 
-const decoratedDebounce = debounce(clickMe, 300, option = { leading:true, trailing: true });
+const decoratedDebounce = debounce(
+  clickMe,
+  300,
+  (option = { leading: true, trailing: true })
+);
 
-button.addEventListener('click', decoratedDebounce);
+button.addEventListener("click", decoratedDebounce);
